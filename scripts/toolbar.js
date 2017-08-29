7ebbccd527f31c2a1c6c4cc1627dfa6c62610343
Ram <quadrupleslap@gmail.com>
2017-08-30 04:18:12 +1000

    HELP ME

--- scripts/toolbar.js
+++ scripts/toolbar.js
@@ -6,6 +6,12 @@ window.TOOLBAR = {
             let x = cel('a');
             x.textContent = text;
             x.href = href;
+
+            if (location.pathname.startsWith(href)) {
+                //TODO: A horrible hack indeed.
+                x.classList.add('current');
+            }
+            
             return x;
         };
 
@@ -14,17 +20,16 @@ window.TOOLBAR = {
         el.appendChild(logo);
         
         el.appendChild(a('Classes', '/classes'));
-        el.appendChild(a('Match', '/matches'));
 
         if (BACKEND.can(PERMISSIONS.READ_DETAILS)) {
-            el.appendChild('Users', '/users');
+            el.appendChild(a('Users', '/users'));
         }
+
+        el.appendChild(a('Match', '/matches'));
 
         let id = BACKEND.id();
 
         if (id !== undefined) {
-            el.appendChild(a('Profile', `/users/view?id=${id}`));
-
             let logout = a('Logout', '#');
             logout.addEventListener('click', ev => {
                 ev.preventDefault();
